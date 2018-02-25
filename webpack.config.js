f0eee827f2b36bbb26f4f30cab579b2d97d2663d
omarglez <omargonzalez233@gmail.com>
2018-02-25 14:42:52 -0500

    Added ignore file and fast development setup

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,11 @@ module.exports = {
                 use: 'babel-loader'
             }
         ]
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000,
+        ignored: /node_modules/
     }
 };
